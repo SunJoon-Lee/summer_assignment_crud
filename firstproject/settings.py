9e436bf069a50ffe5cb489907c6a2cecdab2d446
SunJoon-Lee <tjswns1002@gmail.com>
2019-07-29 01:37:18 +0900

    heroku prac

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = '39#h6v#um9!l$9s)94y12#=r^tmiw(0u#v*fflt$@pms3bf#9*'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '39#h6v#um9!l$9s)94y12#=r^tmiw(0u#v*fflt$@pms3bf#9*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
